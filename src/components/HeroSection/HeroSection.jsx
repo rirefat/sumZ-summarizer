214f5414d874ca435e49673cbaf3633152c017f2
rirefat <rirefat.official@gmail.com>
2024-02-05 11:37:36 +0600

    done navbar

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,9 +1,22 @@
+import { logo } from '../../assets';
 
 const HeroSection = () => {
     return (
-        <div>
-            HeroSection
-        </div>
+        <header className='w-full flex justify-center items-center flex-col'>
+            <nav className="flex justify-between items-center w-full mb-10 pt-3">
+                <img
+                    src={logo}
+                    alt="sumZ logo"
+                    className='w-28 object-contain'
+                />
+
+                <button
+                    type='button'
+                    onClick={() => window.open('https://github.com/rirefat')}
+                    className='black_btn'
+                >Github</button>
+            </nav>
+        </header>
     );
 };
 
